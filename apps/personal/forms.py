517745e849d2b2c80149f6846543e882e9c17325
zhengquantao <1483906080@qq.com>
2020-04-01 19:13:02 +0800

    *

--- apps/personal/forms.py
+++ apps/personal/forms.py
@@ -6,9 +6,41 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
-from .models import WorkOrder, WorkOrderRecord
+from .models import WorkOrder, WorkOrderRecord, Order
 
 User = get_user_model()
+
+
+class OrderForm(forms.ModelForm):
+    """
+    订单检查表
+    """
+    class Meta:
+        model = Order
+        fields = ["status", "order_number", "remark", "system_sku", "img", "product_chinese_name", "finish_status", "comparison_code", "purchase_quantity", "purchase_link", "operation", "operation_manager", "sales_30"]
+        error_messages = {
+            "system_sku": {"required": "请输入SKU"},
+            "order_number": {"required": "请输入订单号"},
+            "status": {"required": "请输入状态"},
+            "product_chinese_name": {"required": "请输入商品名"},
+            "comparison_code": {"required": "请输入对照码"},
+            "finish_status": {"required": "请输入品类"},
+            "purchase_link": {"required": "请输入采购链接"},
+            "sales_30": {"required": "请输入最近30天销量"},
+            "purchase_quantity": {"required": "请输入订单数量"},
+            "remark": {"required": "请输入备注内容"},
+            "operation": {"required": "请选择提交人"},
+            "operation_manager": {"required": "请选择审核人"},
+        }
+
+
+class OrderCreateForm(forms.ModelForm):
+    """
+    多余的
+    """
+    class Meta:
+        model = Order
+        fields = '__all__'
 
 
 class ImageUploadForm(forms.ModelForm):
@@ -32,9 +64,7 @@ class WorkOrderCreateForm(forms.ModelForm):
             "title": {"required": "请输入工单标题"},
             "type": {"required": "请选择工单类型"},
             "status": {"required": "请选择工单状态"},
-            "do_time": {"required": "请输入工单安排时间"},
             "content": {"required": "请输入工单内容"},
-            "customer": {"required": "请选客户信息"},
         }
 
     def clean(self):
@@ -52,17 +82,23 @@ class WorkOrderUpdateForm(forms.ModelForm):
         model = WorkOrder
         fields = '__all__'
         error_messages = {
-            "title": {"required": "请输入工单标题"},
-            "type": {"required": "请选择工单类型"},
-            "status": {"required": "请选择工单状态"},
-            "do_time": {"required": "请输入工单安排时间"},
-            "content": {"required": "请输入工单内容"},
-            "customer": {"required": "请选客户信息"},
+            "system_sku": {"required": "请输入SKU"},
+            "order_number": {"required": "请输入订单号"},
+            "status": {"required": "请输入状态"},
+            "product_chinese_name": {"required": "请输入商品名"},
+            "comparison_code": {"required": "请输入对照码"},
+            "finish_status": {"required": "请输入品类"},
+            "purchase_link": {"required": "请输入采购链接"},
+            "sales_30": {"required": "请输入最近30天销量"},
+            "purchase_quantity": {"required": "请输入订单数量"},
+            "remark": {"required": "请输入备注内容"},
+            "operation": {"required": "请选择提交人"},
+            "operation_manager": {"required": "请选择审核人"},
         }
 
     def clean(self):
         cleaned_data = super(WorkOrderUpdateForm, self).clean()
-        approver = cleaned_data.get("approver", "")
+        approver = cleaned_data.get("operation_manager", "")
         if not approver:
             raise forms.ValidationError("请选择工单审批人")
 
--- gistandard/urls.py
+++ gistandard/urls.py
@@ -66,13 +66,4 @@ urlpatterns = [
     url(r'^personal/workorder_all/$', order.WorkOrderView.as_view(), name="personal-workorder_all"),
     url(r'^personal/document/$', order.WorkOrderDocumentView.as_view(), name="personal-document"),
     url(r'^personal/document/list', order.WorkOrderDocumentListView.as_view(), name="personal-document-list"),
-
-
-    url(r'^personal/order_Icrt/$', order.OrderView.as_view(), name="personal-order_Icrt"),
-    url(r'^personal/order_Icrt/list$', order.OrderListView.as_view(), name="personal-order-list"),
-    url(r'^personal/order_Icrt/create$', order.OrderCreateView.as_view(), name="personal-order-create"),
-    url(r'^personal/order_Icrt/detail', order.OrderDetailView.as_view(), name="personal-order-detail"),
-    url(r'^personal/order_Icrt/delete', order.OrderDeleteView.as_view(), name="personal-order-delete"),
-    url(r'^personal/order_Icrt/update', order.OrderUpdateView.as_view(), name="personal-order-update"),
-
 ]
